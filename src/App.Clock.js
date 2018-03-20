71c7783b82f5607b5c69bc56b16053856a4a9eca
Liviu Ioan Iancu <liviu.iancu@tim-mb-150.3pillar.corp>
2018-03-20 13:18:29 +0200

    Handle event for simple increase count button

--- src/App.Clock.js
+++ src/App.Clock.js
@@ -4,8 +4,10 @@ export class AppClock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date()
+      date: new Date(),
+      clickCount: 0
     };
+    this.increaseCount = this.increaseCount.bind(this);
   }
 
   componentDidMount() {
@@ -17,6 +19,11 @@ export class AppClock extends Component {
   componentWillUnmount() {
     clearInterval(this.timerId);
   }
+  increaseCount() {
+    this.setState((prevState) => ({
+      clickCount: prevState.clickCount + 1 
+    }));
+  }
   tick() {
     this.setState({
       date: new Date()
@@ -27,6 +34,7 @@ export class AppClock extends Component {
       <div>
         <h1>Hello, world!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button class="btn btn-success" onClick={this.increaseCount}>Count {this.state.clickCount}</button>
       </div>
     );
   }
